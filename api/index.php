d7a59d3ad2f8e66ef452ce097248725d00195df7
Fikri <fikrighifari17.com>
2026-05-15 02:03:54 +0700

    Fix Error 500 by creating storage structure in /tmp

--- api/index.php
+++ api/index.php
@@ -1,4 +1,19 @@
 <?php
 
+// Create required storage directories in /tmp
+$storageDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/bootstrap/cache',
+    '/tmp/storage/logs',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 // Forward Vercel requests to normal index.php
 require __DIR__ . '/../public/index.php';
